c7eb78b394b14d85c776beeb7a9a5e48b64cbc1d
Luc Myers <myers.457@wright.edu>
2026-04-13 13:07:08 -0400

    Added functions to Formula

--- Formula.java
+++ Formula.java
@@ -1,23 +1,87 @@
-public class Formula <T> {
+import java.util.List;
 
-    public boolean P(T a, T b){
-        return a.equals(b);
-    }
+public class Formula<T>{
 
-    public boolean S(T a){
-        return a != null;
-    }
-
-    public boolean A(T a){
-        return false;
-        
-    }
+    private final List<T> universe;
     
-    public int function(int x, int y){
-        return x*y;
-        
+    public Formula(List<T> universe){
+        this.universe = universe;
+
     }
 
+
+    public  boolean P(T x, T y){
+        return x.equals(y);
+    }
+
+    public  boolean S(T y){
+        return y != null;
+    }
+
+    public  boolean A(T x){
+        return x != null;
+    }
+
+    public boolean forEveryX(){
+        /*TODO: Change Output
+        Default: false
+        */
+        
+        for (T x : universe){
+             if(!thereExistsY(x))return false;
+
+        }
+        return true;
+
+    }
+
+    
+    public boolean thereExistsY(T x){
+        
+        for(T y:universe){
+
+            if (implication(x, y)){
+
+
+                 if(forEveryZ(x,y)){
+                    return false;
+                 }
+
+            }            
+        }
+
+    }
+
+    public boolean forEveryZ(T x, T y) {
+        for(T z : universe){
+            if(!zCondition(x,y,z)){
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+    public boolean implication(T x,T y){
+        return P(x, y) && A(x) && S(y);
+
+    }
+
+
+
+    public boolean zCondition(T x, T y, T z){
+        return ((P(y,z) && A(x)));
+
+    }
+
+
+    
+
+
+    public boolean evaluate() {
+        
+        return forEveryX();
+    }
 
 
 }
