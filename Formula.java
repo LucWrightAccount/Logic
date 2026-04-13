ad57d38c7a57c37acb8866efb8b2885e08196acd
Luc Myers <myers.457@wright.edu>
2026-04-12 23:40:18 -0400

    first commit

---
+++ Formula.java
@@ -0,0 +1,17 @@
+public class Formula <T> {
+
+    public boolean P(T a, T b){
+        return a.equals(b);
+    }
+
+    public boolean S(T a){
+        return a != null;
+    }
+
+    public boolean A(){
+        
+    }
+
+
+
+}
